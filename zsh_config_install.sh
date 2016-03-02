a27fa519e2fd3b1e29f193d3fb24cd86460d3d30
Eric Maniez <e.maniez@weezevent.com>
2016-03-02 21:48:38 +0100

    update

--- zsh_config_install.sh
+++ zsh_config_install.sh
@@ -6,5 +6,5 @@ mv -v .gitconfig .zsh_save/.gitconfig
 ln -s zsh_config/.zsh .zsh
 ln -s zsh_config/.zshrc .zshrc
 ln -s zsh_config/.gitconfig .gitconfig
-git config --global core.excludesfile zsh_config/.gitignore_global
+git config --global core.excludesfile ~/zsh_config/.gitignore_global
 echo "\033[0;32mInstallation succesful!\033[0m"
