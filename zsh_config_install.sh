005b6be26b123467970c10304938b407c9aa158c
Eric Maniez <e.maniez@weezevent.com>
2016-03-07 12:14:05 +0100

    absolute filepaths

--- zsh_config_install.sh
+++ zsh_config_install.sh
@@ -1,10 +1,10 @@
 cd ~
-mkdir -p .zsh_save
+mkdir -p ~/.zsh_save
 mv -v .zshrc .zsh_save/.zshrc
 mv -v .zsh .zsh_save/.zsh
 mv -v .gitconfig .zsh_save/.gitconfig
-ln -s zsh_config/.zsh .zsh
-ln -s zsh_config/.zshrc .zshrc
-ln -s zsh_config/.gitconfig .gitconfig
+ln -s ~/zsh_config/.zsh .zsh
+ln -s ~/zsh_config/.zshrc .zshrc
+ln -s ~/zsh_config/.gitconfig .gitconfig
 git config --global core.excludesfile ~/zsh_config/.gitignore_global
 echo "\033[0;32mInstallation succesful!\033[0m"
